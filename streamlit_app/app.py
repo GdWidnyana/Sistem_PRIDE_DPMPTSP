713c0d517d50c9766fa2e0eec19afa294b69f842
GdWidnyana <120539660+GdWidnyana@users.noreply.github.com>
2025-09-04 23:02:54 +0800

    Update app.py

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -8,19 +8,19 @@ from prediksi import prediksi
 # Set page config di sini
 st.set_page_config(page_title="Sistem Analisa dan Prediksi")
 
-# # Menampilkan logo di sidebar
-# st.sidebar.image("streamlit_app/resources/dpmptsp.png", use_column_width=True)
+# Menampilkan logo di sidebar
+st.sidebar.image("streamlit_app/resources/Logo_Badung.png", use_column_width=True)
 
 # Menampilkan heading di sidebar dengan rata tengah dan tautan ke LinkedIn
-st.sidebar.markdown(
-    """
-    <div style="text-align: center;">
-        <h1>Sistem PRIDE</h1>
-        <h2><a href="https://www.linkedin.com/in/i-gede-widnyana/" target="_blank" style="text-decoration: none; color: inherit;">oleh I Gede Widnyana</a></h2>
-    </div>
-    """, 
-    unsafe_allow_html=True
-)
+# st.sidebar.markdown(
+#     """
+#     <div style="text-align: center;">
+#         <h1>Sistem PRIDE</h1>
+#         <h2><a href="https://www.linkedin.com/in/i-gede-widnyana/" target="_blank" style="text-decoration: none; color: inherit;">oleh I Gede Widnyana</a></h2>
+#     </div>
+#     """, 
+#     unsafe_allow_html=True
+# )
 
 # Inisialisasi state sesi
 if 'loggedIn' not in st.session_state:
@@ -40,8 +40,8 @@ else:
         analisa_data()
     elif page == "Prediksi":
         prediksi()
-    elif page == "Profil":
-        profil()
+    # elif page == "Profil":
+    #     profil()
 
     # Button untuk logout
     if st.sidebar.button("Logout"):
